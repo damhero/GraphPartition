cbae545a79816b7756bfc428f19270713f321c5e
Wojciech Ziembowicz <143935791+w-ziem@users.noreply.github.com.143935791+w-ziem@users.noreply.github.com>
2025-05-15 18:43:09 +0200

    fix conflicts

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import com.example.model.Graph;
 import com.example.ui.Frame;
+import com.example.utils.CSRRGParser;
 
 public class App {
     public static void main(String[] args) {
--- src/main/java/com/example/model/Graph.java
+++ src/main/java/com/example/model/Graph.java
@@ -1,16 +1,34 @@
 package com.example.model;
 
+import java.util.ArrayList;
+
 public class Graph {
     private int vertexCount;
-    private int [] adjacencyList;
-    private int [] adjacencyIndices;
+    private ArrayList<Integer> adjacencyList;
+    private ArrayList<Integer> adjacencyIndices;
 
     //konstruktor
-    public Graph(int vertexCount, int [] adjacencyList, int [] adjacencyIndices) {
+    public Graph(int vertexCount, ArrayList<Integer> adjacencyList, ArrayList<Integer> adjacencyIndices) {
         this.vertexCount = vertexCount;
         this.adjacencyList = adjacencyList;
         this.adjacencyIndices = adjacencyIndices;
     }
+
+    //gettery
+
+    public int getVertexCount() {
+        return vertexCount;
+    }
+
+    public ArrayList<Integer> getAdjacencyList() {
+        return adjacencyList;
+    }
+
+    public ArrayList<Integer> getAdjacencyIndices() {
+        return adjacencyIndices;
+    }
+
+
 
 
 
